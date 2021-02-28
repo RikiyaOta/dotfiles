84eb92e57a3c2e76766848f3993550b34f2e47a6
rikiyaota <rikiya0421.fcb6@gmail.com>
2021-02-28 12:41:22 +0900

    Fix.

--- install.sh
+++ install.sh
@@ -6,6 +6,7 @@ echo "Creating symbolic link now..."
 
 CURRENT_DIR=$(pwd)
 
+# For dot files in root dir.
 for f in .??*
 do
     [[ "$f" == ".config" ]] && continue
@@ -15,6 +16,15 @@ do
     ln -sf $CURRENT_DIR/$f ~/$f
 done
 
+# For universal ctags
+mkdir -p ~/.ctags.d/
+for f in $(ls '.ctags.d' | grep ".ctags")
+do
+  echo $f
+  ln -sf $CURRENT_DIR/.ctags.d/$f ~/.ctags.d/$f
+done
+
+# For git
 mkdir -p ~/.config/git/
 ln -sf $CURRENT_DIR/.config/git/ignore ~/.config/git/ignore
 
