f90cb32fd71394df61ea9e2b2141e89d015395d0
rikiyaota <rikiya0421.fcb6@gmail.com>
2022-06-20 14:36:09 +0900

    Install lsp package.

--- .config/nvim/lua/plugins.lua
+++ .config/nvim/lua/plugins.lua
@@ -1,9 +1,7 @@
 vim.cmd[[packadd packer.nvim]]
 
-require'packer'.startup(function()
 local use = require('packer').use
 require('packer').startup(function()
   use 'wbthomason/packer.nvim' -- Package manager
   use 'neovim/nvim-lspconfig' -- Configurations for Nvim LSP
-end)
 end)
