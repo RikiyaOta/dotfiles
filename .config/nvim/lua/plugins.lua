3248d07692caa8a3de1b1f9f805293cc2fa19e2c
rikiyaota <rikiya0421.fcb6@gmail.com>
2022-06-21 11:02:44 +0900

    Update

--- .config/nvim/lua/plugins.lua
+++ .config/nvim/lua/plugins.lua
@@ -1,7 +1,10 @@
 vim.cmd[[packadd packer.nvim]]
 
-local use = require('packer').use
 require('packer').startup(function()
   use 'wbthomason/packer.nvim' -- Package manager
   use 'neovim/nvim-lspconfig' -- Configurations for Nvim LSP
+  use 'williamboman/nvim-lsp-installer'
+  use 'nvim-lua/plenary.nvim'
+  use "jose-elias-alvarez/null-ls.nvim"
+  use "jose-elias-alvarez/nvim-lsp-ts-utils"
 end)
